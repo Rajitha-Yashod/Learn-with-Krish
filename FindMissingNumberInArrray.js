1fb67159d27f43f2c075098d80191858eaca9ef7
Rajitha Yashod <75484003+Rajitha-Yashod@users.noreply.github.com>
2022-06-03 22:09:29 +0530

    Update FindMissingNumberInArrray.js
    Update

--- FindMissingNumberInArrray.js
+++ FindMissingNumberInArrray.js
@@ -1,4 +1,5 @@
 
+
 //sum is the missing number;
 function getMissingNumber(arr,n){
 
@@ -7,10 +8,25 @@ function getMissingNumber(arr,n){
      sum-=arr[i];
 
      var missingNumber=sum;
+     //console.log(missingNumber);
+         
+     if(missingNumber < arr[n - 1]){ // missed a number but not first or last number
          return missingNumber;
+     }else if (missingNumber%2==0 && n%2==1){ //number of array element is odd and last namber is missed
+         return missingNumber;
+     }else if(missingNumber%2==1 && n%2==1){ //number of array element is odd and first namber is missed
+         return (missingNumber-(n+1));
+     }else if(missingNumber%2 ==0 && n%2 ==0){ //number of array element is even and first namber is missed
+         return (missingNumber-(n+1));
+     }else if(missingNumber%2 == 1 && n%2 ==0){ //number of array element is even and last namber is missed
+        return missingNumber;
+         return missingNumber;
+     }
+
 }
 
- var inputArray =[26,24,25,22,21,27,29,28,30];
+ //var inputArray =[21,23,24,25,26,27,28,29,30]; // 9-elements
+ var inputArray =[22,23,24,21,25,27,28,29];     // 8-elements
  inputArray.sort();
 
 let arrayLength= inputArray.length;
